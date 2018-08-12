0b17b592d6e7486661b4cd3155a16befee1e6fc8
ng3thanh <ngthanh2093@gmail.com>
2018-08-12 15:12:01 +0700

    update code

--- app/Http/Requests/BlogPostRequest.php
+++ app/Http/Requests/BlogPostRequest.php
@@ -24,15 +24,15 @@ class BlogPostRequest extends FormRequest
     public function rules()
     {
         return [
-            'trans.vn.title' => 'required|max:200',
-            'trans.vn.slug' => 'required|max:200',
-            'trans.vn.description' => 'required|max:1000',
-            'trans.vn.content' => 'required|max:20000',
-            'trans.en.title' => 'required|max:200',
-            'trans.en.slug' => 'required|max:200',
-            'trans.en.description' => 'required|max:1000',
-            'trans.en.content' => 'required|max:20000',
-            'image' => 'required'
+//            'trans.vn.title' => 'required|max:200',
+//            'trans.vn.slug' => 'required|max:200',
+//            'trans.vn.description' => 'required|max:1000',
+//            'trans.vn.content' => 'required|max:20000',
+//            'trans.en.title' => 'required|max:200',
+//            'trans.en.slug' => 'required|max:200',
+//            'trans.en.description' => 'required|max:1000',
+//            'trans.en.content' => 'required|max:20000',
+//            'image' => 'required'
         ];
     }
 }
