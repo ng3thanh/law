b0670543ee54a90df793f22e2fd6e381ea39e075
ng3thanh <ngthanh2093@gmail.com>
2018-08-12 13:12:01 +0700

    update code

--- resources/views/web/partials/connect/facebook.blade.php
+++ resources/views/web/partials/connect/facebook.blade.php
@@ -1,63 +1,12 @@
-<!-- Load Facebook SDK for JavaScript -->
 <div id="fb-root"></div>
-<script>
-    window.fbAsyncInit = function() {
-        FB.init({
-            appId            : '2074718712602436',
-            autoLogAppEvents : true,
-            xfbml            : true,
-            version          : 'v2.11'
-        });
-    };
-    (function(d, s, id){
+<script>(function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {return;}
+        if (d.getElementById(id)) return;
         js = d.createElement(s); js.id = id;
-        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.5";
         fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-</script>
+    }(document, 'script', 'facebook-jssdk'));</script>
 
-<!-- Your customer chat code -->
-<div class="fb-customerchat"
-     page_id="2091885660824738"
-     theme_color="#0084ff"
-     logged_in_greeting="Hi! How can we help you?"
-     logged_out_greeting="GoodBye!... Hope to see you soon."
-     minimized="false">
-</div>
-
-<!-- Load Facebook SDK for JavaScript -->
-{{--<div id="fb-root"></div>--}}
-{{--<script>--}}
-    {{--window.fbAsyncInit = function() {--}}
-        {{--FB.init({--}}
-            {{--appId            : '2074718712602436',--}}
-            {{--autoLogAppEvents : true,--}}
-            {{--xfbml            : true,--}}
-            {{--version          : 'v2.12'--}}
-        {{--});--}}
-    {{--};--}}
-    {{--(function(d, s, id) {--}}
-        {{--var js, fjs = d.getElementsByTagName(s)[0];--}}
-        {{--if (d.getElementById(id)) return;--}}
-        {{--js = d.createElement(s); js.id = id;--}}
-        {{--js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1';--}}
-        {{--fjs.parentNode.insertBefore(js, fjs);--}}
-    {{--}(document, 'script', 'facebook-jssdk'));</script>--}}
-
-{{--<!-- Your customer chat code -->--}}
-{{--<div class="fb-customerchat" page_id="183866149051455" theme_color="#0084ff"></div>--}}
-
-{{--<div id="fb-root"></div>--}}
-{{--<script>(function(d, s, id) {--}}
-        {{--var js, fjs = d.getElementsByTagName(s)[0];--}}
-        {{--if (d.getElementById(id)) return;--}}
-        {{--js = d.createElement(s); js.id = id;--}}
-        {{--js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.5";--}}
-        {{--fjs.parentNode.insertBefore(js, fjs);--}}
-    {{--}(document, 'script', 'facebook-jssdk'));</script>--}}
-
-{{--<div style="position:fixed; z-index:9999999; right:10px; bottom:10px;" class="fb-page" data-tabs="messages"--}}
-     {{--data-href="https://www.facebook.com/Im-183866149051455/" data-width="250" data-height="300"--}}
-     {{--data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" data-show-posts="false"></div>--}}
+<div style="position:fixed; z-index:9999999; right:10px; bottom:10px;" class="fb-page" data-tabs="messages"
+     data-href="https://www.facebook.com/pg/Thanh-test-2091885660824738/" data-width="250" data-height="300"
+     data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" data-show-posts="false"></div>
