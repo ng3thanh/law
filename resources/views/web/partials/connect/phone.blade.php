2fc6c67045b20c41d1a22d4c59c4ed17dea7cbbd
ng3thanh <ngthanh2093@gmail.com>
2018-08-12 12:00:40 +0700

    update code

--- resources/views/web/partials/connect/phone.blade.php
+++ resources/views/web/partials/connect/phone.blade.php
@@ -1,3 +1,3 @@
-<a href="tel:+84936200593" class="scrollup">
-    <i class="icon-angle-up icon-square icon-bgdark icon-2x"></i>
+<a href="tel:+84936200593" id="phone-call-web">
+    <i class="fa fa-phone-square fa-3x"></i>
 </a>
