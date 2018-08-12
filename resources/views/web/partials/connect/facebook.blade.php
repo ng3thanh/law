b5be465b20438b5d813ca7bf80dce35653236787
ng3thanh <ngthanh2093@gmail.com>
2018-08-12 13:09:49 +0700

    update code

--- resources/views/web/partials/connect/facebook.blade.php
+++ resources/views/web/partials/connect/facebook.blade.php
@@ -1,18 +1,30 @@
 <!-- Load Facebook SDK for JavaScript -->
 <div id="fb-root"></div>
-<script>(function(d, s, id) {
+<script>
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId            : '2074718712602436',
+            autoLogAppEvents : true,
+            xfbml            : true,
+            version          : 'v2.11'
+        });
+    };
+    (function(d, s, id){
         var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
+        if (d.getElementById(id)) {return;}
         js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1';
+        js.src = "https://connect.facebook.net/en_US/sdk.js";
         fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));</script>
+    }(document, 'script', 'facebook-jssdk'));
+</script>
 
 <!-- Your customer chat code -->
 <div class="fb-customerchat"
-     attribution=setup_tool
      page_id="2091885660824738"
-     theme_color="#0084ff">
+     theme_color="#0084ff"
+     logged_in_greeting="Hi! How can we help you?"
+     logged_out_greeting="GoodBye!... Hope to see you soon."
+     minimized="false">
 </div>
 
 <!-- Load Facebook SDK for JavaScript -->
