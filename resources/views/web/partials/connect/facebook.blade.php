d7b0c826695d1544ff67f14dd933ae72c9c23bfb
ng3thanh <ngthanh2093@gmail.com>
2018-08-12 12:46:37 +0700

    update code

--- resources/views/web/partials/connect/facebook.blade.php
+++ resources/views/web/partials/connect/facebook.blade.php
@@ -1,24 +1,41 @@
 <!-- Load Facebook SDK for JavaScript -->
 <div id="fb-root"></div>
-<script>
-    window.fbAsyncInit = function() {
-        FB.init({
-            appId            : '2074718712602436',
-            autoLogAppEvents : true,
-            xfbml            : true,
-            version          : 'v2.12'
-        });
-    };
-    (function(d, s, id) {
+<script>(function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
         js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1';
+        js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1';
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));</script>
 
 <!-- Your customer chat code -->
-<div class="fb-customerchat" attribution="setup_tool" page_id="183866149051455" theme_color="#0084ff"></div>
+<div class="fb-customerchat"
+     attribution=setup_tool
+     page_id="2091885660824738"
+     theme_color="#0084ff">
+</div>
+
+<!-- Load Facebook SDK for JavaScript -->
+{{--<div id="fb-root"></div>--}}
+{{--<script>--}}
+    {{--window.fbAsyncInit = function() {--}}
+        {{--FB.init({--}}
+            {{--appId            : '2074718712602436',--}}
+            {{--autoLogAppEvents : true,--}}
+            {{--xfbml            : true,--}}
+            {{--version          : 'v2.12'--}}
+        {{--});--}}
+    {{--};--}}
+    {{--(function(d, s, id) {--}}
+        {{--var js, fjs = d.getElementsByTagName(s)[0];--}}
+        {{--if (d.getElementById(id)) return;--}}
+        {{--js = d.createElement(s); js.id = id;--}}
+        {{--js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1';--}}
+        {{--fjs.parentNode.insertBefore(js, fjs);--}}
+    {{--}(document, 'script', 'facebook-jssdk'));</script>--}}
+
+{{--<!-- Your customer chat code -->--}}
+{{--<div class="fb-customerchat" page_id="183866149051455" theme_color="#0084ff"></div>--}}
 
 {{--<div id="fb-root"></div>--}}
 {{--<script>(function(d, s, id) {--}}
