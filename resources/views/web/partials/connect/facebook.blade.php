a1eba3cd0baf20195d6879543516d89c51f015b4
ng3thanh <ngthanh2093@gmail.com>
2018-08-12 12:31:06 +0700

    update code

--- resources/views/web/partials/connect/facebook.blade.php
+++ resources/views/web/partials/connect/facebook.blade.php
@@ -15,9 +15,20 @@
         js = d.createElement(s); js.id = id;
         js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1';
         fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-</script>
+    }(document, 'script', 'facebook-jssdk'));</script>
 
 <!-- Your customer chat code -->
-<div class="fb-customerchat" attribution=setup_tool page_id="183866149051455" theme_color="#0084ff">
-</div>
+<div class="fb-customerchat" attribution="setup_tool" page_id="183866149051455" theme_color="#0084ff"></div>
+
+{{--<div id="fb-root"></div>--}}
+{{--<script>(function(d, s, id) {--}}
+        {{--var js, fjs = d.getElementsByTagName(s)[0];--}}
+        {{--if (d.getElementById(id)) return;--}}
+        {{--js = d.createElement(s); js.id = id;--}}
+        {{--js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.5";--}}
+        {{--fjs.parentNode.insertBefore(js, fjs);--}}
+    {{--}(document, 'script', 'facebook-jssdk'));</script>--}}
+
+{{--<div style="position:fixed; z-index:9999999; right:10px; bottom:10px;" class="fb-page" data-tabs="messages"--}}
+     {{--data-href="https://www.facebook.com/Im-183866149051455/" data-width="250" data-height="300"--}}
+     {{--data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" data-show-posts="false"></div>--}}
