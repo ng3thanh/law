540048dec365d2d73eb5440e49e594e3b8e77de2
ng3thanh <ngthanh2093@gmail.com>
2018-08-12 16:26:56 +0700

    update code

--- resources/lang/en/homepage.php
+++ resources/lang/en/homepage.php
@@ -10,7 +10,7 @@ return [
     'our_blog' => 'Our Blog',
     'read_more' => 'Read more',
     'more_blog' => 'More blog post',
-    'feedback_label' => 'Get in Inquiry',
+    'feedback_label' => 'Get a Inquiry',
     'feedback_message' => 'Reque facer nostro et ius, cu persius mnesarchum disputando eam, clita prompta et mel vidisse phaedrum pri et. Facilisis posidonium ex his. Mutat iudico vis in, mea aeque tamquam scripserit an, mea eu ignota viderer probatus. Lorem legere consetetur ei eum. Sumo aeque assentior te eam, pri nominati posidonium consttuam',
     'feedback_message_success' => 'Your message has been sent. Thank you!',
     'your_name' => 'Your name',
