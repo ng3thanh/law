e875daeaba4e79c791249d2aea9ed1932ca426cd
Thanh Nguyen <Thanh.NguyenBa@Nashtechglobal.com>
2018-08-21 17:05:41 +0700

    update code

--- resources/views/web/pages/blogs/list.blade.php
+++ resources/views/web/pages/blogs/list.blade.php
@@ -2,121 +2,126 @@
 
 @section('title', 'Blogs')
 @section('css')
-
+    <link rel="stylesheet" href="{{ asset('web/css/blog.list.css') }}">
 @endsection
 
 @section('content')
-    <div class="container header-padding-top">
-        <div class="row">
-            <!-- Latest Posts -->
-            <main class="posts-listing col-lg-12">
-                <div class="container">
-                    <div class="row">
+    <div id="blog-list">
+        <div class="container header-padding-top">
+            <div class="row" id="blog-title-all">Blog list</div>
+            <div class="row" id="blog">
+                <!-- Latest Posts -->
+                <main class="posts-listing col-lg-12">
+                    <div class="container">
+                        <div class="row">
                         @foreach($blogList as $blog)
-                        <!-- post -->
+                            <!-- post -->
                             <div class="post col-xl-4">
-                                <div class="post-thumbnail">
-                                    <a href="{{ route('blogs.detail', ['id' => $blog->blogs_id, 'slug' => $blog->slug]) }}">
-                                        <img src='{{ asset("$blog->image") }}' alt="{{ $blog->title }}" class="img-fluid" width="100%">
-                                    </a>
-                                </div>
-                                <div class="post-details">
-                                    <div class="post-meta d-flex justify-content-between">
-                                        <div class="date meta-last">{{ dateFormat($blog->created_at, "F j| Y") }}</div>
-                                        <div class="category">
-                                            <a href="#">{{ breakStringToArray($blog->tags)[0] ?? '' }}</a>
-                                        </div>
-                                    </div>
-                                    <a href="{{ route('blogs.detail', ['id' => $blog->id, 'slug' => $blog->slug]) }}">
-                                        <h3 class="h4">{{ $blog->title }}</h3>
-                                    </a>
-                                    <p class="text-muted">
-                                        {!! (strlen($blog->description) > 350) ? substr($blog->description, 0, 347) . ' ... ' : $blog->description !!}
-                                    </p>
-                                    <a href="{{ route('blogs.detail', ['id' => $blog->id, 'slug' => $blog->slug]) }}" class="more">{{ __('homepage.continue_reading') }}</a>
-                                    <footer class="post-footer d-flex align-items-center">
-                                        <a href="#" class="author d-flex align-items-center flex-wrap">
-                                            <div class="avatar">
-                                                <img src="{{ asset('web/img/admin_logo.png') }}" alt="Admin" class="img-fluid">
-                                            </div>
-                                            <div class="title">
-                                                <span>{{ $blog->author }}</span>
-                                            </div>
+                                <div class="pre-content">
+                                    <div class="post-thumbnail blog-image">
+                                        <a href="{{ route('blogs.detail', ['id' => $blog->blogs_id, 'slug' => $blog->slug]) }}">
+                                            <img src='{{ asset("$blog->image") }}' alt="{{ $blog->title }}" class="img-fluid" width="100%">
                                         </a>
-                                        <div class="date">
-                                            <i class="icon-clock"></i> {{ timeElapsedString($blog->created_at) }}
+                                    </div>
+                                    <div class="post-details blog-detail">
+                                        <div class="post-meta d-flex justify-content-between">
+                                            <div class="date meta-last blog-created">{{ dateFormat($blog->created_at, "F j| Y") }}</div>
+                                            <div class="category blog-tag">
+                                                <a href="#">{{ breakStringToArray($blog->tags)[0] ?? '' }}</a>
+                                            </div>
                                         </div>
-                                        <div class="views meta-last">
-                                            <i class="icon-eye"></i> {{ $blog->view }}
-                                        </div>
-                                    </footer>
+                                        <a class="blog-title" href="{{ route('blogs.detail', ['id' => $blog->id, 'slug' => $blog->slug]) }}">
+                                            <h3 class="h4">{{ $blog->title }}</h3>
+                                        </a>
+                                        <p class="text-muted blog-content">
+                                            {!! (strlen($blog->description) > 350) ? substr($blog->description, 0, 347) . ' ... ' : $blog->description !!}
+                                        </p>
+                                        <a href="{{ route('blogs.detail', ['id' => $blog->id, 'slug' => $blog->slug]) }}" class="blog-more">{{ __('homepage.continue_reading') }}</a>
+                                        <footer class="post-footer d-flex align-items-center">
+                                            <a href="#" class="author d-flex align-items-center flex-wrap">
+                                                <div class="avatar">
+                                                    <img src="{{ asset('web/img/admin_logo.png') }}" alt="Admin" class="img-fluid">
+                                                </div>
+                                                <div class="title">
+                                                    <span>{{ $blog->author }}</span>
+                                                </div>
+                                            </a>
+                                            <div class="date">
+                                                <i class="icon-clock"></i> {{ timeElapsedString($blog->created_at) }}
+                                            </div>
+                                            <div class="views meta-last">
+                                                <i class="icon-eye"></i> {{ $blog->view }}
+                                            </div>
+                                        </footer>
+                                    </div>
                                 </div>
                             </div>
-                        @endforeach
-                    </div>
-                    <!-- Pagination -->
+                            @endforeach
+                        </div>
+                        <!-- Pagination -->
 
-                    <nav aria-label="Page navigation example">
-                        @if (isset($blogList) && $blogList->lastPage() > 1)
-                            <ul class="pagination pagination-template d-flex justify-content-center">
+                        <nav aria-label="Page navigation example">
+                            @if (isset($blogList) && $blogList->lastPage() > 1)
+                                <ul class="pagination pagination-template d-flex justify-content-center">
                                 @php
-                                $interval = isset($interval) ? abs(intval($interval)) : 3 ;
-                                $from = $blogList->currentPage() - $interval;
-                                if($from < 1){
-                                    $from = 1;
-                                }
+                                    $interval = isset($interval) ? abs(intval($interval)) : 3 ;
+                                    $from = $blogList->currentPage() - $interval;
+                                    if($from < 1){
+                                        $from = 1;
+                                    }
 
-                                $to = $blogList->currentPage() + $interval;
-                                if($to > $blogList->lastPage()){
-                                    $to = $blogList->lastPage();
-                                }
+                                    $to = $blogList->currentPage() + $interval;
+                                    if($to > $blogList->lastPage()){
+                                        $to = $blogList->lastPage();
+                                    }
                                 @endphp
 
 
                                 <!-- first/previous -->
-                                @if($blogList->currentPage() > 1)
-                                    <li class="page-item">
-                                        <a href="{{ $blogList->url(1) }}" class="page-link">
-                                            <i class="fa fa-angle-double-left"></i>
-                                        </a>
-                                    </li>
+                                    @if($blogList->currentPage() > 1)
+                                        <li class="page-item">
+                                            <a href="{{ $blogList->url(1) }}" class="page-link">
+                                                <i class="fa fa-angle-double-left"></i>
+                                            </a>
+                                        </li>
 
-                                    <li class="page-item">
-                                        <a href="{{ $blogList->url($blogList->currentPage() - 1) }}" class="page-link">
-                                            <i class="fa fa-angle-left"></i>
-                                        </a>
-                                    </li>
-                                @endif
+                                        <li class="page-item">
+                                            <a href="{{ $blogList->url($blogList->currentPage() - 1) }}" class="page-link">
+                                                <i class="fa fa-angle-left"></i>
+                                            </a>
+                                        </li>
+                                    @endif
 
                                 <!-- links -->
-                                @for($i = $from; $i <= $to; $i++)
-                                    @php
-                                        $isCurrentPage = $blogList->currentPage() == $i;
-                                    @endphp
-                                    <li class="page-item">
-                                        <a href="{{ !$isCurrentPage ? $blogList->url($i) : '#' }}" class="page-link {{ $isCurrentPage }}">{{ $i }}</a>
-                                    </li>
-                                @endfor
+                                    @for($i = $from; $i <= $to; $i++)
+                                        @php
+                                            $isCurrentPage = $blogList->currentPage() == $i;
+                                        @endphp
+                                        <li class="page-item">
+                                            <a href="{{ !$isCurrentPage ? $blogList->url($i) : '#' }}" class="page-link {{ $isCurrentPage }}">{{ $i }}</a>
+                                        </li>
+                                    @endfor
 
                                 <!-- next/last -->
-                                @if($blogList->currentPage() < $blogList->lastPage())
-                                    <li class="page-item">
-                                        <a href="{{ $blogList->url($blogList->currentPage() + 1) }}" class="page-link">
-                                            <i class="fa fa-angle-right"></i>
-                                        </a>
-                                    </li>
+                                    @if($blogList->currentPage() < $blogList->lastPage())
+                                        <li class="page-item">
+                                            <a href="{{ $blogList->url($blogList->currentPage() + 1) }}" class="page-link">
+                                                <i class="fa fa-angle-right"></i>
+                                            </a>
+                                        </li>
 
-                                    <li class="page-item">
-                                        <a href="{{ $blogList->url($blogList->lastpage()) }}" class="page-link">
-                                            <i class="fa fa-angle-double-right"></i>
-                                        </a>
-                                    </li>
-                                @endif
-                            </ul>
-                        @endif
-                    </nav>
-                </div>
-            </main>
+                                        <li class="page-item">
+                                            <a href="{{ $blogList->url($blogList->lastpage()) }}" class="page-link">
+                                                <i class="fa fa-angle-double-right"></i>
+                                            </a>
+                                        </li>
+                                    @endif
+                                </ul>
+                            @endif
+                        </nav>
+                    </div>
+                </main>
+            </div>
         </div>
     </div>
 @endsection
