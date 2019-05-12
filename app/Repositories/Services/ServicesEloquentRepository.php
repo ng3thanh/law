2d5f11cd3c3b34f436737f893b32d283f07037ce
ng3thanh <ngthanh2093@gmail.com>
2019-05-12 20:08:58 +0700

    fix error

--- app/Repositories/Services/ServicesEloquentRepository.php
+++ app/Repositories/Services/ServicesEloquentRepository.php
@@ -23,11 +23,15 @@ class ServicesEloquentRepository extends BaseEloquentRepository implements Servi
      */
     public function findBySlugRelatedId($id)
     {
+
         $locale = app()->getLocale();
-        $result = $this->model->join('services_translate', 'services_translate.services_id', '=', 'services.id')
+        $result = $this->model
+            ->leftJoin('services_translate', 'services_translate.services_id', '=', 'services.id')
             ->where('services_translate.services_id', $id)
             ->where('services_translate.locale', $locale)
-            ->first();
+            ->first()
+            ->getAttributes();
+        $result['author'] = $locale == 'vi' ? 'Quản trị viên' : 'Administrator';
         return $result;
     }
 }
--- resources/views/web/partials/connect/phone.blade.php
+++ resources/views/web/partials/connect/phone.blade.php
@@ -1,3 +1,3 @@
-<a href="tel:+84936200593" id="phone-call-web">
+<a href="tel:+84962690696" id="phone-call-web">
     <img src="{{ asset('web/img/phone.png') }}" alt="Phone call" width="50px" />
 </a>
